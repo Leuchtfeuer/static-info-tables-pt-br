c06b58bd3eb9e90a006d04236f24b2dc4d759f1a
Guido Schmechel <info@ayacoo.de>
2019-10-02 23:07:44 +0200

    Property type for shortNamePrBr

--- Classes/Domain/Model/Country.php
+++ Classes/Domain/Model/Country.php
@@ -5,13 +5,22 @@ namespace SJBR\StaticInfoTables\Domain\Model;
 
 class Country extends AbstractEntity
 {
+    /**
+     * @var string
+     */
     protected $shortNamePtBr = '';
 
+    /**
+     * @return string
+     */
     public function getShortNamePtBr(): string
     {
         return $this->shortNamePtBr;
     }
 
+    /**
+     * @param string $shortNamePtBr
+     */
     public function setShortNamePtBr(string $shortNamePtBr)
     {
         $this->shortNamePtBr = $shortNamePtBr;
